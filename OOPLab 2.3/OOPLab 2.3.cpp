4761346daaa79ef6340c8963246aa3e553929ac6
Rumpl3slaw <wowpanda13@ukr.net>
2021-04-04 13:47:29 +0300

    x

---
+++ OOPLab 2.3/OOPLab 2.3.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include "Rational.h"
+
+using namespace std;
+
+int main()
+{
+	 SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	Rational a(17, 2, 33, 4);
+
+	++a;
+	cout << a;
+	cout<<endl;
+	--a;
+	cout << a;
+	a++;
+	cout << a;
+	a--;
+	cout << a;
+	cout << endl;
+	cout << a.operator string();
+	Rational b(17,2,33,4);
+	cin >> b;
+	cout << b;
+	cout << endl;
+	cout << b.operator string();
+	b.Dodavannya();
+	cout<<endl;
+	b.Mnojennya();
+	cout << endl;
+	b.Vidnimannya();
+	cout << endl;
+	b.Value();
+
+
+
+
+}
